a681c430c1d3ae29a5ea7f0f33d89608521314e7
liushaokang <3336724921@qq.com>
2021-03-06 11:23:50 +0800

    测试reset

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use TCG\Voyager\Facades\Voyager;
 | contains the "web" middleware group. Now create something great!
 |
 */
+               
+
 
 Route::get('/', function () {
     return view('welcome');
